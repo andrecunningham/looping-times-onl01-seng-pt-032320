379cd624e2f274662776964b21e6aef76a33c2f1
Andre Cunningham <andrecunningham62@gmail.com>
2020-03-26 19:29:42 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 def using_times
 
 7.times do
 	  puts "Wingardium Leviosa"
 	  
 	end
+=======
+def useing_times
+Wingardium Leviosa = 7
+	7.times do
+	  puts "Wingardium Leviosa"
+>>>>>>> a3a4417a235559ae06db8cc1dbe21b8b5041208b
 end
 
 
